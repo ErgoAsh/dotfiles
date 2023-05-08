df8a3b87c2dc294d21b5b6d249a4d5d4d753abdc
Marek K <ErgoAsh@users.noreply.github.com>
2023-05-08 17:08:28 +0200

    Improve kitty in multi-tab mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,6 +46,7 @@ if status is-interactive
     set fish_cursor_visual      block
 
     fzf_configure_bindings --git_status=\cs --git_log=\cg --history=\ch --variables=\cv --processes=\cp --directory=\cf 
+    
 end
 
 if status is-login
